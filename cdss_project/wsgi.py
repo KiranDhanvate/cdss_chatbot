75843cae75211e2a3de3c027478a8eeb3967c02d
KiranDhanvate <kirandhanvate735@gmail.com>
2025-03-20 12:16:07 +0530

    build changes

--- cdss_project/wsgi.py
+++ cdss_project/wsgi.py
@@ -8,10 +8,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cdss_project.settings')
+
+# Collect static files for Vercel deployment
+if os.environ.get('VERCEL') == '1':
+    from django.core.management import call_command
+    try:
+        call_command('collectstatic', '--noinput', '--clear')
+    except Exception as e:
+        print(f"Error collecting static files: {e}")
 
 application = get_wsgi_application()
 
